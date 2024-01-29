ea2e804d2ef9c94f7ea6758cfc650c5590cbb21b
Nityanand Singh <nns.nitya@gmail.com>
2024-01-29 17:27:25 +0530

    stream: passing pradicate in filter()
    pradicate=boolean valued function, passed in filter method

---
+++ stream-api/src/work/stream/Main.java
@@ -0,0 +1,34 @@
+package work.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Q) Create a list and filter all even numbers from list
+		
+		List<Integer> list1 = List.of(2,4, 50, 55, 33, 12, 17);
+		
+		List<Integer> list2 = new ArrayList<>();
+		list2.add(12);
+		list2.add(26);
+		list2.add(57);
+		list2.add(72);
+		
+		List<Integer> list3 = Arrays.asList(23, 11, 37, 12, 99, 38);
+		
+		//list1
+		//using stream
+		Stream<Integer> stream = list1.stream();
+		List<Integer> listEven = stream.filter(i -> i%2==0).collect(Collectors.toList());
+		System.out.println(list1);
+		System.out.println(listEven);
+		
+	}
+
+}
