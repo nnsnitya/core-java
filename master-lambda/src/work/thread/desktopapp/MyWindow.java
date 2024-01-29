50294b5d39d5da4f0b9615bec5b0b64ca3dead00
Nityanand Singh <nns.nitya@gmail.com>
2024-01-29 16:58:20 +0530

    lambda not useful added anonymous class
    MouseMotionListener is not a functional interface so here we can not use lambda, need to use anonymous class to impl it.

--- master-lambda/src/work/thread/desktopapp/MyWindow.java
+++ master-lambda/src/work/thread/desktopapp/MyWindow.java
@@ -1,8 +1,11 @@
 package work.thread.desktopapp;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -34,9 +37,21 @@ public class MyWindow {
 			JOptionPane.showMessageDialog(null, "Hey!, Button is clicked.");
 		});
 		
+		button.addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				button.setForeground(Color.ORANGE);
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+			}
+		});
+		
 		frame.add(button);
-		
-		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
