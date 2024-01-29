0221094de4d03140a983ef50c04adcfc5d95d919
Nityanand Singh <nns.nitya@gmail.com>
2024-01-29 16:37:46 +0530

    lambda: anonymous class impls Action Listener
    Action Listener interface(functional interface )is implemented by anonymous class

--- master-lambda/src/work/thread/desktopapp/MyWindow.java
+++ master-lambda/src/work/thread/desktopapp/MyWindow.java
@@ -1,10 +1,12 @@
 package work.thread.desktopapp;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MyWindow {
 	public static void main(String[] args) {
@@ -16,6 +18,17 @@ public class MyWindow {
 		
 		//create button and add on jframe
 		JButton button = new JButton("Click Me");
+		
+		//implemented ActionListener(Functional interface) by anonymous class
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button Clicked");
+				JOptionPane.showMessageDialog(null, "Hey!, Button is clicked.");
+			}
+		}); 
+		
 		frame.add(button);
 		
 		
