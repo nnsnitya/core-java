50c25aec87da25b6747f315d0665111bf3a9b874
Nityanand Singh <nns.nitya@gmail.com>
2024-02-02 02:26:49 +0530

    singleton pattern broken by reflection

---
+++ JavaPrograms/src/dp/creational/singleton/TestBreakSingleton.java
@@ -0,0 +1,25 @@
+package dp.creational.singleton;
+
+import java.lang.reflect.Constructor;
+
+public class TestBreakSingleton {
+
+	public static void main(String[] args) throws Exception {
+	
+		/*
+		 *1. Reflection API to break singleton pattern
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		DataSource1 dSource1 = DataSource1.getDataSource1();
+		System.out.println(dSource1.hashCode());
+		
+		//to use reflection API, need to load DataSource1 class
+		Constructor<DataSource1> constructor = DataSource1.class.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		DataSource1 dSource2 = constructor.newInstance();
+		System.out.println(dSource2.hashCode());
+	}
+}
