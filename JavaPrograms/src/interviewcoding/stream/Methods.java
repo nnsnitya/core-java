ffec7c9df43f410495ffca7f726a04ce4b7e389c
Nityanand Singh <nns.nitya@gmail.com>
2024-02-05 17:31:38 +0530

    interview stream frequency
    Find Frequency of each element in An Array
    Find Frequency of each element in A String

--- JavaPrograms/src/interviewcoding/stream/Methods.java
+++ JavaPrograms/src/interviewcoding/stream/Methods.java
@@ -2,12 +2,15 @@ package interviewcoding.stream;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Methods {
 	public static void main(String[] args) {
 		
-		List<String> names = List.of("Durgesh", "Aman", "Abhilekh", "Manoj", "Ankit");
+		List<String> names = List.of("Durgesh", "Aman", "Abhilekh", "Durgesh", "Manoj", "Ankit");
 		List<String> newNames = names.stream().filter(e -> e.startsWith("A")).collect(Collectors.toList());
 		System.out.println(newNames);
 		
@@ -23,5 +26,24 @@ public class Methods {
 		System.out.println(distinctList);
 		
 		//"Nityanand".chars().mapToObj(null)
+		
+		
+		System.out.println("---------Frequency of each element in An Array----------");
+		Map<String, Long> frqOfStrList_map = names.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		frqOfStrList_map.entrySet().stream().forEach(e -> {
+			System.out.println("Key: "+e.getKey());
+			System.out.println("Value: "+e.getValue());
+			System.out.println("-------------------");
+		});
+
+		System.out.println("---------Frequency of each element in A String----------");
+		String str = "Abhilekh";
+		Map<Character, Long> frqOfStr_map = str.chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		frqOfStr_map.entrySet().stream().forEach(entry -> {
+			System.out.println("Key -> "+entry.getKey());
+			System.out.println("Value->"+entry.getValue());
+			System.out.println("-------------------");
+		});
+		
 	}
 }
