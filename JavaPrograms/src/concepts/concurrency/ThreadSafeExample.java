00e43bc0dcd7090b2d611048718dc766030020cb
Nityanand Singh <nns.nitya@gmail.com>
2024-02-12 19:32:35 +0530

    concurrency: CopyOnWriteArrayList
    implementation of List interface i.e. Thread Safe class

---
+++ JavaPrograms/src/concepts/concurrency/ThreadSafeExample.java
@@ -0,0 +1,46 @@
+package concepts.concurrency;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ThreadSafeExample {
+
+	public static void main(String[] args) {
+
+		List<String> list = new CopyOnWriteArrayList<>();
+		list.add("First");
+		list.add("Second");
+		list.add("Third");
+		list.add("Fourth");
+		list.add("Fifth");
+		list.add("First");
+		list.add("First");
+		list.add("First");
+		list.add("First");
+		
+		
+		Runnable t1 = () -> {
+			//thread1 task
+			for(String str: list) {
+				System.out.println(str);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		Runnable t2 = () -> {
+			//thread2 task
+			list.add("new item1");
+			list.add("new item2");
+			list.add("new item3");
+		};
+		
+		new Thread(t1).start();
+		new Thread(t2).start();
+	}
+
+}
